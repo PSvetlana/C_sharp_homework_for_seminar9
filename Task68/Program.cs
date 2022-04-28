c60695849db444aa76df7708c7587c2326b4625e
Svetlana Popova <sspopova10-08@yandex.ru>
2022-04-28 20:27:43 +0300

    Task 68 completed

---
+++ Task68/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+    m = 2, n = 3 -> A(m,n) = 9
+    m = 3, n = 2 -> A(m,n) = 29
+    m = 3, n = 3 -> A(m,n) = 61
+-----------------------------------------
+Функция Аккермана:
+          n + 1                  m = 0
+A(m, n) = A(m - 1, 1)            m > 0, n = 0
+          A(m - 1, A(m, n - 1)), m > 0, n > 0   
+*/
+
+int СalculatingAckermanFunction(int numberM, int numberN)
+{
+    if (numberM == 0)
+    {
+        return numberN + 1;
+    }
+    if (numberN == 0)
+    {
+        return СalculatingAckermanFunction(numberM - 1, 1);
+    }
+    return СalculatingAckermanFunction(numberM - 1, СalculatingAckermanFunction(numberM, numberN - 1));
+}
+
+void CheckingUserNumbers(int numberM, int numberN)
+{
+    if ((numberM >= 0) && (numberN >= 0))
+    {
+        Console.Write("Результат вычисления функции Аккермана = ");
+        Console.WriteLine(СalculatingAckermanFunction(numberM, numberN));
+    }
+    else
+    {
+        Console.WriteLine("Числа m, n должны быть больше или равны нулю!");
+    }
+}
+
+Console.Write("Введите число m: ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("--------------------");
+CheckingUserNumbers(userNumberM, userNumberN);
